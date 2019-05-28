76f188b6b54c130d1be9993a3044aaa1ea558de6
Lukie Kang <lukiek+f@gmail.com>
2019-05-28 16:52:14 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -49,6 +49,6 @@ def sum_array(arr) #Use inject to sum all numbers in this array
 end
 
 def add_s(arr) # Add "s" to all elements of the array except the 2nd;
-  newArr = arr.each_with_index.collect{|element, index| index != 1 ? element}
+  newArr = arr.each_with_index.collect{|element, index| index != 1 ? element : }
 puts newArr
 end
